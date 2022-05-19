83015345bc31ac56dfd6ac2bff630fd81f74353e
valleykid <valleykiddy@gmail.com>
2022-05-19 15:47:55 +0800

    feat: 添加必要的中间件

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import { register, start } from '@satumjs/core';
+import { register, start, use } from '@satumjs/core';
+import {
+  simpleSandboxMidware,
+  mountNodeMidware,
+} from '@satumjs/simple-midwares';
+import singleSpaMidware from '@satumjs/midware-single-spa';
+
+use(simpleSandboxMidware);
+use(mountNodeMidware);
+use(singleSpaMidware);
+
 export { register, start };
