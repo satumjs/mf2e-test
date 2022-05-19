1733d7ab54e1090f7fb0bffe71cd780834c5a01a
valleykid <valleykiddy@gmail.com>
2022-05-19 15:55:10 +0800

    feat: 添加微应用本地调试工具

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import { register, start, use } from '@satumjs/core';
+import { register, start, use, MidwareName } from '@satumjs/core';
 import {
   simpleSandboxMidware,
   mountNodeMidware,
 } from '@satumjs/simple-midwares';
 import singleSpaMidware from '@satumjs/midware-single-spa';
+
+use((system, microApps, next) => {
+  system.set(
+    MidwareName.proxyEntry,
+    (entry: string | string[], appName: string) => {
+      const proxyMap = {};
+      const proxySetting = localStorage.getItem('proxyEntries') || '';
+      const proxyData = proxySetting.replace(/\s/g, '').split(',');
+
+      proxyData.forEach((item) => {
+        const [itemAppName, proxyUrl] = item.split('|');
+        if (itemAppName && proxyUrl) proxyMap[itemAppName] = proxyUrl;
+      });
+
+      const currentApp = microApps.find((item) => item.name === appName);
+      return currentApp && proxyMap[appName] ? proxyMap[appName] : entry;
+    }
+  );
+  next();
+});
 
 use(simpleSandboxMidware);
 use(mountNodeMidware);
